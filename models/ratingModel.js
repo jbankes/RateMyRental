375600790bf2a5f2cb3c0dc77251247e16cad9ac
Justin Bankes <jbankes@mail.csuchico.edu>
2016-09-19 11:31:08 -0700

    Added address and started amenities
    Added address to the schema. Started on amenities but questions came
    up.

--- models/ratingModel.js
+++ models/ratingModel.js
@@ -2,6 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ratingSchema = new Schema({
+    address: [
+      streetNum: {type: Number, required: true },
+      streetName: {type: String, required: true},
+      streetType: {type: String, required: true},
+      zipCode: {type: Number, require: true},
+      city: {type: String, require: true},
+      country: {type: String, require: true}
+    ],
+
+    amenities: [
+      washer: {type: Boolean, default: false}, //Should this be a string with a
+      //dropdown as the answer on the set up page?
+    ]
 });
 
 mongoose.model('Rating', ratingSchema);
