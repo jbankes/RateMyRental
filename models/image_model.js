4fe566fcf767c6c69de0268a0a9aae7fa4daa084
Justin Bankes <jbankes@mail.csuchico.edu>
2016-09-19 12:46:18 -0700

    review, image, and rating model started
    Initial set up of rating_model file. Added new model files for the
    embedded documents in mongoose. This will allow us to do embedded
    documents and add multiple reviews to a single property.

---
+++ models/image_model.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var image_schema = new Schema({
+
+});
+
+mongoose.model('Review', image_schema)
---
+++ models/review_model.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var review_schema = new Schema({
+
+});
+
+mongoose.model('Review', review_schema)
