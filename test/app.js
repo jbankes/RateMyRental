8fef1767d1b4bb242a8a31592dd0cca7b2440800
Justin Bankes <jbankes@mail.csuchico.edu>
2016-09-28 12:27:55 -0700

    Set up test for homepage

--- test/app.js
+++ test/app.js
@@ -1,2 +1,13 @@
 var assert = require('assert')
 var rate_my_rental = require('..')
+
+describe('homepage', function(){
+  it('should respond to GET', function(){
+    superagent
+      .get('http://localhost:'+port)
+      end(function(res){
+        expect(res.status).to.equal(200);
+        done();
+      })
+  })
+})
