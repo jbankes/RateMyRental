fbae6137e9c203b0828ad86f94b468691dd17c38
Justin Bankes <jbankes@mail.csuchico.edu>
2016-09-28 13:45:23 -0700

    Fixed home page Get Unit Test

--- test/app.js
+++ test/app.js
@@ -1,11 +1,12 @@
+var request = require('superagent')
 var assert = require('assert')
-var rate_my_rental = require('..')
+// var rate_my_rental = require('../RateMyRental')
 
 describe('homepage', function(){
   it('should respond to GET', function(){
-    superagent
-      .get('http://localhost:'+port)
-      end(function(res){
+    request
+      .get('http://localhost:8000')
+      .end(function(res){
         expect(res.status).to.equal(200);
         done();
       })
