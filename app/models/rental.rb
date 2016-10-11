3b7c80a4209ca5274562aba9f7576af6700b8dd7
kdavis99 <kdavis99@mail.csuchico.edu>
2016-10-11 03:01:35 -0700

    static + dynamic maps are working, unstyled

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,13 @@
 class Rental < ActiveRecord::Base
 	has_many :reviews, dependent: :destroy
 	has_many :images, dependent: :destroy
+
+	geocoded_by :full_address
+	after_validation :geocode
+
+	def full_address
+		# street = addr_street_num.to_s + " " + addr_street_name
+		[addr_street_num.to_s + " " + addr_street_name, addr_city, addr_country].compact.join(', ')
+	end
+
 end
