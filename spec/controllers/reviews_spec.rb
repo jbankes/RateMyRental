acdab26bd30350b9434c5fe13b1c9b18fb6b428c
Matt Nachtigal <mnachtigal@mail.csuchico.edu>
2016-11-09 12:45:50 -0800

    Review testing

--- spec/controllers/reviews_spec.rb
+++ spec/controllers/reviews_spec.rb
@@ -1,5 +1,6 @@
 # Reviews Controller Spec
 require 'rails_helper'
+require 'rental'
 
 RSpec.describe ReviewsController, :type => :controller do
   describe 'GET #index' do
@@ -14,4 +15,24 @@ RSpec.describe ReviewsController, :type => :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe 'GET #show' do
+    it 'responds succesfully with 200 status'do
+    get :show
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #new rental review'do
+  it 'creates new review'do
+    get :new
+    FactoryGirl.create(:rental).should_be_valid
+    rental.reviews.new
+  
+    expect(response).to be_a_new(rental.reviews.new)
+  end
+end
+
+
 end
